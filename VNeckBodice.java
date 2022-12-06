5ff05fa2b079559f9e22f73072962d743e293e30
Alette Ng <85789376+AletteXin@users.noreply.github.com>
2022-12-06 17:53:22 -0500

    Update VNeckBodice.java

--- VNeckBodice.java
+++ VNeckBodice.java
@@ -15,31 +15,29 @@ public class VNeckBodice extends Bodice {
 	/**
 	 * Set constructor for VNeckBodice 
 	 */
-	public VNeckBodice(int hip, int waist, int neckDepth, int shoulder, int armHoleDepth, int napeToWaist, int neckCir) {
+	public VNeckBodice(int hip, int waist, int neckDepth, int shoulder, int armHoleDepth, 
+			int napeToWaist, int neckCir) {
+		//use constructor formula from Bodice class 
 		super(hip, waist, neckDepth, shoulder, armHoleDepth, napeToWaist, neckCir);
 	}
 	
-	
+	/**
+	 * Draw front neck 
+	 */
 	@Override 
-	public void drawFrontNeck(GeneralPath typeOfBodice, int frontNeckXOne, int frontNeckYOne, int frontNeckXTwo, int frontNeckYTwo, int frontNeckXThree, int frontNeckYThree) {
-		
+	public void drawFrontNeck(GeneralPath typeOfBodice, int frontNeckXOne, int frontNeckYOne, 
+			int frontNeckXTwo, int frontNeckYTwo, int frontNeckXThree, int frontNeckYThree) {
 		typeOfBodice.lineTo(frontNeckXThree, frontNeckYThree);	
-
 	}
 
-	
+	/**
+	 * Provides a string which describes the bodice type 
+	 * @return String with bodice type description
+	 */
 	@Override
 	public String getBodiceType() {
 		return "V Neck Bodice";
 	}
-	
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		
-		
-	}
 
 }
