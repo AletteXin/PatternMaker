0ad64e68d80c462aeff81335bbb0d5b0ffaba80e
Alette Ng <85789376+AletteXin@users.noreply.github.com>
2022-12-06 17:48:45 -0500

    Update CNeckBodice.java

--- CNeckBodice.java
+++ CNeckBodice.java
@@ -9,35 +9,30 @@ import javax.swing.*;
 
 public class CNeckBodice extends Bodice {
 
-	
-	static final String neckLine = "C";
-	
 	/**
 	 * Set constructor for VNeckBodice 
 	 */
 	public CNeckBodice(int hip, int waist, int neckDepth, int shoulder, int armHoleDepth, int napeToWaist, int neckCir) {
+		//use constructor formula from Bodice class 
 		super(hip, waist, neckDepth, shoulder, armHoleDepth, napeToWaist, neckCir);
 	}
-	
-	
+
+	/**
+	 * Draw front neck 
+	 */
 	@Override 
 	public void drawFrontNeck(GeneralPath typeOfBodice, int frontNeckXOne, int frontNeckYOne, int frontNeckXTwo, int frontNeckYTwo, int frontNeckXThree, int frontNeckYThree) {
-		
 		typeOfBodice.curveTo(frontNeckXOne, frontNeckYOne, frontNeckXTwo, frontNeckYTwo, frontNeckXThree, frontNeckYThree);	
-	
-}
+	}
 
+	/**
+	 * Provides a string which describes the bodice type 
+	 * @return String with bodice type description
+	 */
 	@Override
 	public String getBodiceType() {
 		return "C Neck Bodice";
 	}
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		
-		
-	}
 
 }
