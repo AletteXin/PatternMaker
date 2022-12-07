f132a8d8d16e7c45dec1448da0ae10919405514a
Alette Ng <85789376+AletteXin@users.noreply.github.com>
2022-12-07 16:54:13 -0500

    Update BodiceTest.java

--- BodiceTest.java
+++ BodiceTest.java
@@ -19,9 +19,11 @@ class BodiceTest {
 	@Test
 	void testGetBodiceType() {
 		
+		//test 1: c neck bodice 
 		CNeckBodice testCBodice = new CNeckBodice(132, 107, 10, 15, 26, 54, 40);
 		assertEquals("C Neck Bodice", testCBodice.getBodiceType());
 		
+		//test 2: v neck bodice 
 		VNeckBodice testVBodice = new VNeckBodice(132, 107, 10, 15, 26, 54, 40);
 		assertEquals("V Neck Bodice", testVBodice.getBodiceType());
 		
@@ -31,9 +33,12 @@ class BodiceTest {
 	void testGetArmHoleDepth() {
 		
 		int expand = 5;
+	
+		//test 1: when arm hole depth measurement is 26cm
 		CNeckBodice testCBodice = new CNeckBodice(132, 107, 10, 15, 26, 54, 40);
 		assertEquals(26 * expand, testCBodice.getArmHoleDepth());
 		
+		//test 2: when arm hole depth measurement is 24cm 
 		VNeckBodice testVBodice = new VNeckBodice(132, 107, 10, 15, 24, 54, 40);
 		assertEquals(24 * expand, testVBodice.getArmHoleDepth());
 		
